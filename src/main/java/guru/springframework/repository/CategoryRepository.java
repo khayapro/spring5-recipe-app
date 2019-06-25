4d553ecd72f7f2d70bb844bb276efb0aca837346
Khaya Mathebula <khayaprom@gmail.com>
2019-06-25 21:56:01 +0200

    spring stuff

--- src/main/java/guru/springframework/repository/CategoryRepository.java
+++ src/main/java/guru/springframework/repository/CategoryRepository.java
@@ -2,9 +2,14 @@ package guru.springframework.repository;
 
 import guru.springframework.entities.Category;
 import guru.springframework.entities.Recipe;
+import jdk.nashorn.internal.runtime.options.Option;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface CategoryRepository extends JpaRepository<Category, Long> {
+
+    Optional<Category> findByDescription(String description);
 }
--- src/main/java/guru/springframework/repository/UnitMeasureRepository.java
+++ src/main/java/guru/springframework/repository/UnitMeasureRepository.java
@@ -4,6 +4,10 @@ import guru.springframework.entities.UnitMeasure;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UnitMeasureRepository extends JpaRepository<UnitMeasure, Long> {
+
+    Optional<UnitMeasure> findByDescription(String description);
 }
